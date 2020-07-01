692a916598db8a597156d8b45aaee448b48afac9
Ryan Garvey <ryangarvey423@gmail.com>
2020-07-01 22:39:27 +0100

    Complete Priority algorithm

---
+++ src/Priority.cpp
@@ -0,0 +1,64 @@
+#include "headers/Scheduler.h"
+#include "headers/ProcessSorts.h"
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void Priority(vector<Process>& procs) {
+    Scheduler schedule;
+    vector<Process> toExecute;
+
+    sort(procs.begin(), procs.end(), sortByArrival);
+
+    int currTime = procs[0].arrival + procs[0].burst;
+    schedule.add(procs[0], procs[0].arrival);
+
+    for (int i = 1; i < procs.size(); ++i) {
+        sort(toExecute.begin(), toExecute.end(), sortByPriority);
+        //if empty queue
+        if (toExecute.size() == 0) {
+            // push to queue
+            if (currTime > procs[i].arrival) {
+                toExecute.push_back(procs[i]);
+            // execute straight away
+            } else {
+                schedule.add(procs[i], currTime);
+                currTime += procs[i].burst;
+            }
+        // queue has elements
+        } else {
+            // push to queue
+            if (currTime > procs[i].arrival) {
+                toExecute.push_back(procs[i]);
+            // execute front of element, push current element to queue
+            } else {
+                schedule.add(toExecute.front(), currTime);
+                currTime += toExecute.front().burst;
+                toExecute.erase(toExecute.begin());
+
+                toExecute.push_back(procs[i]);
+            }
+        }
+    }
+    
+    while(toExecute.size() > 0) {
+        schedule.add(toExecute.front(), currTime);
+        currTime += toExecute.front().burst;
+        toExecute.erase(toExecute.begin());
+    }
+
+    schedule.printSchedule();
+}
+
+int main() {
+    Process a("A", 0, 10, 10);
+    Process b("B", 4, 6, 11);
+    Process c("C", 7, 2, 12);  
+    Process d("D", 9, 7, 7);  
+    Process e("E", 12, 4, 6);  
+    vector<Process> prc {a,b,c,d,e};
+
+    Priority(prc);
+}
--- src/headers/ProcessSorts.h
+++ src/headers/ProcessSorts.h
@@ -6,3 +6,7 @@ bool sortByArrival(Process a, Process b) {
 bool sortByBurst(Process a, Process b) {
     return (b.burst > a.burst);
 }
+
+bool sortByPriority(Process a, Process b) {
+    return (b.priority < a.priority);
+}
